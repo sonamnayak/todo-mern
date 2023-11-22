f86c626d6e69c741e2d14124621b495dfa1da993
sonamnayak <sonamnayak2710@gmail.com>
2023-11-22 13:38:44 +0530

    removing vercel configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://todo-mern-backend-vliz.onrender.com",
-        changeOrigin: "true"
-      },
-    },
-  },
+  plugins: [react()]
 });
