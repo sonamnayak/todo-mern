a59d706aa82c3776a10c7e06fa60b6bf175b6152
sonamnayak <sonamnayak2710@gmail.com>
2023-11-22 13:21:50 +0530

    proxy server fix

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 
 const app = express();
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
